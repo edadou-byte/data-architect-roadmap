ede85411c7dc9beb7c629d2b801855a84e793a2b
emile <edadou@proton.me>
2026-08-15 22:52:09 +0200

    update prefect.yaml and customer_flow.py

--- Orchestration/Prefect/flows/customer_flow.py
+++ Orchestration/Prefect/flows/customer_flow.py
@@ -1,4 +1,9 @@
 from prefect import flow
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 from tasks.customers_tasks import get_customer_ids, process_customer
 
 @flow
